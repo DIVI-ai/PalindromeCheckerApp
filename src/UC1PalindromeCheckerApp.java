63abcb2fa0d400791806f40a6bcf09daf40b423c
DIVI-ai <divitakapoor22@gmail.com>
2026-02-23 15:23:24 +0530

    UC1 : Add wlecome message module

---
+++ src/UC1PalindromeCheckerApp.java
@@ -0,0 +1,24 @@
+public class UC1PalindromeCheckerApp {
+    // UC1: Application Entry & Welcome Message
+// Palindrome Checker App
+        // Main method - Entry point of the application
+        public static void main(String[] args) {
+
+            // Application Name
+            System.out.println("======================================");
+            System.out.println("     Welcome to Palindrome Checker App");
+            System.out.println("======================================");
+
+            // Application Version
+            System.out.println("Application Version : v1.0");
+
+            // Application Description
+            System.out.println("This application checks whether a given string is a palindrome.");
+
+            // Flow control message
+            System.out.println("Program initialized successfully.");
+
+            System.out.println("======================================");
+        }
+}
+
